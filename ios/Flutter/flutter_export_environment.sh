c369ac072db6a0282042c6e60363fcb151b49111
africavanlife <africavanlife@gmail.com>
2020-04-05 09:15:36 +0100

    ios changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\Program Files\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\Flutter\Campsite_new"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/thevanlifeappafrica/Desktop/campsite_new/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/thevanlifeappafrica/Desktop/campsite_new/campsite"
+export "FLUTTER_TARGET=/Users/thevanlifeappafrica/Desktop/campsite_new/campsite/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=E:\Program Files\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/thevanlifeappafrica/Desktop/campsite_new/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
